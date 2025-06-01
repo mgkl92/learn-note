bd275b30d6e548a74b4fee00524f0f2c061f6b87
mgkl3932 <1620426133@qq.com>
2025-06-01 19:54:31 +0800

    A possiblly workable thread pool

--- CppConcurrencyProgramming/ThreadPool.cpp
+++ CppConcurrencyProgramming/ThreadPool.cpp
@@ -1,83 +1,84 @@
 #include "ThreadPool.h"
 
-#include <iostream>
-
 namespace mgkl {
-ThreadPool::ThreadPool(size_t nthreads) {
+ThreadPool::ThreadPool(size_t nthreads) :
+    exit_flag(false) {
+    if (nthreads == 0) {
+        nthreads = std::thread::hardware_concurrency();
+    }
+
     for (size_t i = 0; i < nthreads; ++i) {
-        workers.emplace_back([this] {
-            for (;;) {
-                std::function<void()> task;
-
-                {
-                    // 使用 unique_lock 而非 lock_guard
-                    // 由条件变量来控制加锁和释放锁的过程
-                    std::unique_lock<std::mutex> lock(this->mut);
-                    this->cond.wait(lock, [this] {
-                        return this->stop || !this->tasks.empty();
-                    });
-
-                    // 保证任务队列中的所有任务都能够得到执行
-                    if (this->stop && this->tasks.empty()) {
-                        return;
-                    }
-
-                    // 子线程从任务队列中获取单个任务
-                    task = std::move(this->tasks.front());
-                    this->tasks.pop();
-                }
-
-                // 子线程执行任务
-                task();
-            }
-        });
+        auto worker = std::thread(&ThreadPool::work, this);
+        workers.push_back(std::move(worker));
     }
-}
-
-template <class F, class... Args>
-auto ThreadPool::push(F &&f, Args &&...args) -> std::future<typename std::result_of<F(Args...)>::type> {
-    using return_type = typename std::result_of<F(Args...)>::type;
-
-    // 绑定任务与参数列表
-    // auto task = std::make_shared<std::packaged_task<return_type()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
-    auto task = std::make_unique<std::packaged_task<return_type()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
-    std::future<return_type> res = task->get_future();
-    {
-        std::unique_lock<std::mutex> lock(mut);
-        if (stop) {
-            throw std::runtime_error("ThreadPool stoped.");
-        }
-
-        // tasks.emplace([task]() { (*task)(); });
-        task.push(std::move(task));
-    }
-
-    cond.notify_one();
-    return res;
 }
 
 ThreadPool::~ThreadPool() {
     {
-        std::unique_lock<std::mutex> lock(mut);
-        stop = true;
+        std::lock_guard<std::mutex> lock(mut);
+        exit_flag = true;
     }
+
     cond.notify_all();
+
     for (auto &worker : workers) {
-        worker.join();
+        if (worker.joinable()) {
+            worker.join();
+        }
     }
+}
+
+void ThreadPool::work() {
+    while (true) {
+        std::function<void()> task;
+        {
+            std::unique_lock<std::mutex> lock(mut, std::defer_lock);
+            cond.wait(lock, [&] {
+                return exit_flag || !tasks.empty();
+            });
+
+            if (exit_flag && tasks.empty()) {
+                return;
+            }
+
+            task = std::move(tasks.front());
+            tasks.pop();
+        }
+        task();
+    }
+}
+
+template <typename F, typename... Args>
+auto ThreadPool::submit(F &&f, Args &&...args) -> std::future<typename std::result_of<F(Args...)>::type> {
+    using return_type = typename std::result_of<F(Args...)>::type;
+    auto task_ptr = std::make_shared<std::packaged_task<return_type()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+    auto res = task_ptr->get_future();
+    {
+        std::lock_guard<std::mutex> loclck(mut);
+        tasks.emplace([task_ptr]() {
+            (*task_ptr)();
+        });
+    }
+
+    cond.notify_one();
+
+    return res;
 }
 
 } // namespace mgkl
 
-int main() {
-    auto f = []() { return 1; };
-    std::queue<std::function<void()>> q;
-    auto task = std::make_shared<std::packaged_task<int()>>(f);
-    std::future<int> res = task->get_future();
-    q.emplace([task]() { (*task)(); });
+#include <iostream>
 
-    q.front()();
-    q.pop();
-    std::cout << "result of task : " << res.get() << std::endl;
+int main(int argc, char const *argv[]) {
+    auto f = [](int i) {
+        std::cout << "Id is " << i << std::endl;
+        return 0;
+    };
+    mgkl::ThreadPool pool(2);
+
+    for (size_t i = 0; i < 10; ++i) {
+        pool.submit(f, i);
+    }
+
     return 0;
 }
--- CppConcurrencyProgramming/ThreadPool.h
+++ CppConcurrencyProgramming/ThreadPool.h
@@ -1,44 +1,33 @@
 #pragma once
 
-#include <vector>
-#include <queue>
-#include <memory>
-#include <thread>
-#include <condition_variable>
 #include <future>
+#include <vector>
+#include <thread>
 #include <functional>
-#include <stdexcept>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
 
 namespace mgkl {
 class ThreadPool {
 public:
-    // 线程池初始化
-    ThreadPool(size_t nthreads);
+    // 创建并启动线程池
+    ThreadPool(size_t nthreads = 0);
 
-    // 向任务队列中添加任务
-    //
-    template <class F, class... Args>
-    auto push(F &&f, Args &&...args) -> std::future<typename std::result_of<F(Args...)>::type>;
-
-    // 线程池析构
     ~ThreadPool();
 
-private:
-    // 线程池管理的工作线程
-    std::vector<std::thread> workers;
+    template <typename F, typename... Args>
+    auto submit(F &&f, Args &&...args) -> std::future<typename std::result_of<F(Args...)>::type>;
 
-    // 线程池管理的任务队列
-    // FIFO 任务调度策略
+private:   
+    std::vector<std::thread> workers;
+    void work();
+
     std::queue<std::function<void()>> tasks;
 
-    // 主线程与子线程互斥访问任务队列
     std::mutex mut;
-
-    // 条件变量用去唤醒被被阻塞（等待任务）的子线程
     std::condition_variable cond;
 
-    // 线程池启标识
-    bool stop;
+    bool exit_flag;
 };
-
 } // namespace mgkl
